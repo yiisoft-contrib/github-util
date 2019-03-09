4c3aca4195cd30444aee53576ec8f56a080257e3
Alexander Makarov <sam@rmcreative.ru>
2019-03-09 13:09:20 +0300

    Added Yii 2 packages

--- config/packages.php
+++ config/packages.php
@@ -1,10 +1,50 @@
 <?php
 return [
-//    'yii2',
+    'yii2',
+
+    // Yii 2 project templates:
+
+    'yii2-app-basic',
+    'yii2-app-advanced',
+
+
+    // Yii 2 extensions:
+    'yii2-debug',
+    'yii2-twig',
+    'yii2-bootstrap4',
+    'yii2-queue',
+    'yii2-debug',
+    'yii2-elasticsearch',
+    'yii2-sphinx',
+    'yii2-apidoc',
+    'yii2-gii',
+    'yii2-mongodb',
+    'yii2-redis',
+    'yii2-httpclient',
+    'yii2-authclient',
+    'yii2-jui',
+    'yii2-imagine',
+    'yii2-faker',
+    'yii2-smarty',
+    'yii2-collection',
+    'yii2-swiftmailer',
+    'yii2-bootstrap',
+    'yii2-shell',
+
+
+
+    // Yii 2 extras
+    'yii2-docker',
+    'yii2-composer',
+
+
+    // Yii 3
     'yii-core',
     'yii-console',
     'yii-web',
     'yii-rest',
+    'yii-dev',
+    'yii-docker',
 
     'yii-project-template',
     'yii-base-web',
@@ -32,6 +72,7 @@ return [
     'db',
     'active-record',
     'rbac',
+    'view',
 
     'db-mysql',
     'db-mssql',
